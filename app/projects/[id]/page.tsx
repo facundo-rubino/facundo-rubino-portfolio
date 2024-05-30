9d81ee865ad025eeb15630ee7f3c6b2f0a1317c1
facundo-rubino <facundorubino21@gmail.com>
2024-05-30 00:03:31 -0300

    feat: added new animations

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -3,7 +3,9 @@
 import { useParams, useRouter } from 'next/navigation';
 import { projects } from '@/data';
 import React from 'react';
-import { MacbookScroll } from '@/components/ui/MacbookScroll';
+import { AnimatedTooltip } from '@/components/ui/AnimatedTooltip';
+import { ContainerScroll } from '@/components/ui/ContainerScrollAnimation';
+import Image from 'next/image';
 
 const ProjectDetail = () => {
     const params = useParams();
@@ -15,21 +17,29 @@ const ProjectDetail = () => {
 
   return (
     <div>
-            <div className="overflow-hidden dark:bg-[#0B0B0F] bg-white w-full">
-      <MacbookScroll
-        title={
-          <span>
-            This Macbook is built with Tailwindcss. <br /> No kidding.
-          </span>
+          <div className="flex flex-col overflow-hidden">
+      <ContainerScroll
+        titleComponent={
+          <>
+            <h1 className="text-4xl font-semibold text-black dark:text-white">
+               {project!.subTitle}<br/>
+              <span className="text-4xl md:text-[6rem] font-bold mt-1 leading-none">
+              {project!.title} 
+              </span>
+            </h1>
+          </>
         }
-        badge={
-            <Badge className="h-10 w-10 transform -rotate-12" />
-        }
-        src={project!.img}
-        showGradient={false}
-      />
+      >
+        <Image
+          src={project!.img}
+          alt="hero"
+          height={720}
+          width={1400}
+          className="mx-auto rounded-2xl object-cover h-full object-left-top"
+          draggable={false}
+        />
+      </ContainerScroll>
     </div>
-
          <button
         onClick={() => router.back()}
         className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
@@ -39,55 +49,24 @@ const ProjectDetail = () => {
       <h1>{project!.title}</h1>
       <p>{project!.des}</p>
       <img src={project!.img} alt={project!.title} />
-      <div>
+      {/* <div>
         {project!.iconLists.map((icon, index) => (
           <img key={index} src={icon} alt={`icon-${index}`} />
         ))}
-      </div>
+      </div> */}
+      
+      <section className="py-5">
+        
+      <h1 className=" heading text-orange-500">Tecnologías {' '} 
+        <span className="text-slate-700 dark:text-slate-100" >elegidas</span>
+        </h1>
+      <div className="flex flex-row items-center justify-center mb-10 w-full gap-12">
+      <AnimatedTooltip items={project!.techList} />
+    </div>
+        <p className="text-center dark:text-slate-200 py-2">las elegi porque....</p>
+      </section>
     </div>
   );
 };
 
 export default ProjectDetail;
-
-const Badge = ({ className }: { className?: string }) => {
-    return (
-      <svg
-        width="24"
-        height="24"
-        viewBox="0 0 56 56"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        className={className}
-      >
-        <path
-          d="M56 28C56 43.464 43.464 56 28 56C12.536 56 0 43.464 0 28C0 12.536 12.536 0 28 0C43.464 0 56 12.536 56 28Z"
-          fill="#00AA45"
-        ></path>
-        <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
-          d="M28 54C42.3594 54 54 42.3594 54 28C54 13.6406 42.3594 2 28 2C13.6406 2 2 13.6406 2 28C2 42.3594 13.6406 54 28 54ZM28 56C43.464 56 56 43.464 56 28C56 12.536 43.464 0 28 0C12.536 0 0 12.536 0 28C0 43.464 12.536 56 28 56Z"
-          fill="#219653"
-        ></path>
-        <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
-          d="M27.0769 12H15V46H24.3846V38.8889H27.0769C34.7305 38.8889 41 32.9048 41 25.4444C41 17.984 34.7305 12 27.0769 12ZM24.3846 29.7778V21.1111H27.0769C29.6194 21.1111 31.6154 23.0864 31.6154 25.4444C31.6154 27.8024 29.6194 29.7778 27.0769 29.7778H24.3846Z"
-          fill="#24292E"
-        ></path>
-        <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
-          d="M18 11H29.0769C36.2141 11 42 16.5716 42 23.4444C42 30.3173 36.2141 35.8889 29.0769 35.8889H25.3846V43H18V11ZM25.3846 28.7778H29.0769C32.1357 28.7778 34.6154 26.39 34.6154 23.4444C34.6154 20.4989 32.1357 18.1111 29.0769 18.1111H25.3846V28.7778Z"
-          fill="white"
-        ></path>
-        <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
-          d="M17 10H29.0769C36.7305 10 43 15.984 43 23.4444C43 30.9048 36.7305 36.8889 29.0769 36.8889H26.3846V44H17V10ZM19 12V42H24.3846V34.8889H29.0769C35.6978 34.8889 41 29.7298 41 23.4444C41 17.1591 35.6978 12 29.0769 12H19ZM24.3846 17.1111H29.0769C32.6521 17.1111 35.6154 19.9114 35.6154 23.4444C35.6154 26.9775 32.6521 29.7778 29.0769 29.7778H24.3846V17.1111ZM26.3846 19.1111V27.7778H29.0769C31.6194 27.7778 33.6154 25.8024 33.6154 23.4444C33.6154 21.0864 31.6194 19.1111 29.0769 19.1111H26.3846Z"
-          fill="#24292E"
-        ></path>
-      </svg>
-    );
-}
